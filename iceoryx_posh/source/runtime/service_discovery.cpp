536c2b03288caba318f15482584f52bde1f24e80
Simon Hoinkis <simon.hoinkis@apex.ai>
2022-02-11 15:13:06 +0100

    iox-#415 Add ServiceDiscovery test cases using WaitSet and Listener
    Signed-off-by: Simon Hoinkis <simon.hoinkis@apex.ai>

--- iceoryx_posh/source/runtime/service_discovery.cpp
+++ iceoryx_posh/source/runtime/service_discovery.cpp
@@ -58,9 +58,9 @@ void ServiceDiscovery::invalidateTrigger(const uint64_t uniqueTriggerId)
 }
 
 popo::WaitSetIsConditionSatisfiedCallback
-ServiceDiscovery::getCallbackForIsStateConditionSatisfied(const popo::SubscriberState event)
+ServiceDiscovery::getCallbackForIsStateConditionSatisfied(const popo::SubscriberState state)
 {
-    return m_serviceRegistrySubscriber.getCallbackForIsStateConditionSatisfied(event);
+    return m_serviceRegistrySubscriber.getCallbackForIsStateConditionSatisfied(state);
 }
 
 } // namespace runtime
