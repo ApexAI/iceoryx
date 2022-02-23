234113fb73d0183c954b458c0c6b6613e1ef7f83
Dietrich Krönke <dietrich.kroenke@apex.ai>
2022-02-23 11:03:52 +0100

    iox-#27 Rewrite listener example client to use waitset
    Signed-off-by: Dietrich Krönke <dietrich.kroenke@apex.ai>

--- iceoryx_examples/request_response/client_cxx_waitset.cpp
+++ iceoryx_examples/request_response/client_cxx_waitset.cpp
@@ -17,17 +17,20 @@
 //! [iceoryx includes]
 #include "request_and_response_types.hpp"
 
+#include "iceoryx_hoofs/cxx/optional.hpp"
 #include "iceoryx_hoofs/internal/concurrent/smart_lock.hpp"
 #include "iceoryx_hoofs/posix_wrapper/signal_watcher.hpp"
 #include "iceoryx_posh/popo/client.hpp"
-#include "iceoryx_posh/popo/listener.hpp"
-#include "iceoryx_posh/popo/notification_callback.hpp"
+#include "iceoryx_posh/popo/wait_set.hpp"
 #include "iceoryx_posh/runtime/posh_runtime.hpp"
 //! [iceoryx includes]
 
 #include <iostream>
 
-constexpr char APP_NAME[] = "iox-cpp-request-response-client-listener";
+iox::cxx::optional<iox::popo::WaitSet<>> waitset;
+
+constexpr char APP_NAME[] = "iox-cpp-request-response-client-waitset";
+
 
 struct ContextData
 {
@@ -37,55 +40,29 @@ struct ContextData
     int64_t expectedResponseSequenceId = requestSequenceId;
 };
 
-void onResponseReceived(iox::popo::Client<AddRequest, AddResponse>* client,
-                        iox::concurrent::smart_lock<ContextData>* ctx)
-{
-    auto guardedCtx = ctx->getScopeGuard();
-    //! [take response]
-    while (client->take().and_then([&](const auto& response) {
-        auto receivedSequenceId = response.getResponseHeader().getSequenceId();
-        if (receivedSequenceId == guardedCtx->expectedResponseSequenceId)
-        {
-            guardedCtx->fibonacciLast = guardedCtx->fibonacciCurrent;
-            guardedCtx->fibonacciCurrent = response->sum;
-            std::cout << APP_NAME << " Got Response : " << guardedCtx->fibonacciCurrent << std::endl;
-        }
-        else
-        {
-            std::cout << "Got Response with outdated sequence ID! Expected = " << guardedCtx->expectedResponseSequenceId
-                      << "; Actual = " << receivedSequenceId << "! -> skip" << std::endl;
-        }
-    }))
-    {
-    }
-    //! [take response]
-}
+iox::concurrent::smart_lock<ContextData> ctx;
 
 int main()
 {
     //! [initialize runtime]
-    constexpr char APP_NAME[] = "iox-cpp-request-response-client-listener";
     iox::runtime::PoshRuntime::initRuntime(APP_NAME);
     //! [initialize runtime]
 
-    iox::popo::Listener listener;
+    //! [create waitset]
+    waitset.emplace();
 
     //! [create client]
     iox::popo::Client<AddRequest, AddResponse> client({"Example", "Request-Response", "Add"});
     //! [create client]
 
-    iox::concurrent::smart_lock<ContextData> ctx;
+    // attach client to waitset
+    waitset->attachState(client, iox::popo::ClientState::HAS_RESPONSE).or_else([](auto) {
+        std::cerr << "failed to attach client" << std::endl;
+        std::exit(EXIT_FAILURE);
+    });
+    //! [create waitset]
 
-    listener
-        .attachEvent(client,
-                     iox::popo::ClientEvent::RESPONSE_RECEIVED,
-                     iox::popo::createNotificationCallback(onResponseReceived, ctx))
-        .or_else([](auto) {
-            std::cerr << "unable to attach client" << std::endl;
-            std::exit(EXIT_FAILURE);
-        });
-
-    //! [send requests in a loop]
+    //! [mainloop]
     while (!iox::posix::hasTerminationRequested())
     {
         //! [send request]
@@ -106,12 +83,43 @@ int main()
             });
         //! [send request]
 
-        constexpr std::chrono::milliseconds SLEEP_TIME{1000U};
+
+        // We block and wait for samples to arrive.
+        auto notificationVector = waitset->wait();
+
+        for (auto& notification : notificationVector)
+        {
+            if (notification->doesOriginateFrom(&client))
+            {
+                auto guardedCtx = ctx.getScopeGuard();
+                //! [take response]
+                while (client.take().and_then([&](const auto& response) {
+                    auto receivedSequenceId = response.getResponseHeader().getSequenceId();
+                    if (receivedSequenceId == guardedCtx->expectedResponseSequenceId)
+                    {
+                        guardedCtx->fibonacciLast = guardedCtx->fibonacciCurrent;
+                        guardedCtx->fibonacciCurrent = response->sum;
+                        std::cout << APP_NAME << " Got Response : " << guardedCtx->fibonacciCurrent << std::endl;
+                    }
+                    else
+                    {
+                        std::cout << "Got Response with outdated sequence ID! Expected = "
+                                  << guardedCtx->expectedResponseSequenceId << "; Actual = " << receivedSequenceId
+                                  << "! -> skip" << std::endl;
+                    }
+                }))
+                {
+                }
+                //! [take response]
+            }
+        }
+        constexpr std::chrono::milliseconds SLEEP_TIME{950U};
         std::this_thread::sleep_for(SLEEP_TIME);
     }
-    //! [send requests in a loop]
+    //! [mainloop]
 
-    listener.detachEvent(client, iox::popo::ClientEvent::RESPONSE_RECEIVED);
+    std::cout << "shutting down" << std::endl;
 
-    return EXIT_SUCCESS;
+    waitset.reset();
+    return (EXIT_SUCCESS);
 }
